517ae478857b27ea26767b15b6525f574eb1ca88
Alessandro Lemser <lemser.alessandro@gmail.com>
2026-03-31 21:39:13 +0100

    Clenup 3

--- src/oceano-now-playing.py
+++ src/oceano-now-playing.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 
-from app.main import (
-    main,
-    detect_media_player,
-    states_are_equal,
-    should_resolve_artwork,
-    artwork_identity_changed,
-    should_reconnect_player,
-)
-
-__all__ = [
-    "main",
-    "detect_media_player",
-    "states_are_equal",
-    "should_resolve_artwork",
-    "artwork_identity_changed",
-    "should_reconnect_player",
-]
+from app.main import main
 
 
 if __name__ == "__main__":
