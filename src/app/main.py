4df11b906d0026b901758caf171d5e4ae0865135
alemser <lemser.alessandro@gmail.com>
2026-04-01 12:53:23 +0100

    Considering the format for changing the state (#7)

--- src/app/main.py
+++ src/app/main.py
@@ -47,7 +47,7 @@ def states_are_equal(s1, s2):
     if s1 is None or s2 is None:
         return s1 == s2
 
-    keys = ['title', 'artist', 'album', 'status', 'samplerate', 'bitdepth', 'albumart']
+    keys = ['title', 'artist', 'album', 'status', 'samplerate', 'bitdepth', 'albumart', 'playback_source']
     for key in keys:
         if s1.get(key) != s2.get(key):
             return False
